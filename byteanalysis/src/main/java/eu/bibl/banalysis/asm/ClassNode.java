9bfa154e9ab88c7079b895e8d173340641ab0145
Bibl <GenerallyCool@hotmail.com>
2014-08-29 20:51:48 +0100

    Updated breaking ClassNode

--- byteanalysis/src/main/java/eu/bibl/banalysis/asm/ClassNode.java
+++ byteanalysis/src/main/java/eu/bibl/banalysis/asm/ClassNode.java
@@ -1,18 +1,20 @@
 package eu.bibl.banalysis.asm;
 
-import eu.bibl.banalysis.filter.FieldFilter;
-import eu.bibl.banalysis.filter.MethodFilter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.FieldNode;
 import org.objectweb.asm.tree.MethodNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import eu.bibl.banalysis.filter.FieldFilter;
+import eu.bibl.banalysis.filter.MethodFilter;
 
 @SuppressWarnings("all")
 public class ClassNode extends org.objectweb.asm.tree.ClassNode {
 	
 	public ClassNode() {
-		super();
+		super(Opcodes.ASM5);// don't super() it will throw an illegalstateexception
 	}
 	
 	public ClassNode(int asmVersion) {
@@ -25,7 +27,7 @@ public class ClassNode extends org.objectweb.asm.tree.ClassNode {
 	
 	public List<MethodNode> methods(MethodFilter filter) {
 		List<MethodNode> methods = new ArrayList<MethodNode>();
-		for(MethodNode method : methods()) {
+		for (MethodNode method : methods()) {
 			if (filter.accept(method))
 				methods.add(method);
 		}
@@ -38,7 +40,7 @@ public class ClassNode extends org.objectweb.asm.tree.ClassNode {
 	
 	public List<FieldNode> fields(FieldFilter filter) {
 		List<FieldNode> fields = new ArrayList<FieldNode>();
-		for(FieldNode field : fields()) {
+		for (FieldNode field : fields()) {
 			if (filter.accept(field))
 				fields.add(field);
 		}
